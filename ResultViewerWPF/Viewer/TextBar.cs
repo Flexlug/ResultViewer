41e1d8ed1194388e7ea3fc7f0142b135f3e9ee06
Flexlug <flexlug@outlook.com>
2019-03-03 19:31:50 +0300

    1.1.0.2  - Updated version in DevInfo

--- ResultViewerWPF/Viewer/TextBar.cs
+++ ResultViewerWPF/Viewer/TextBar.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ResultViewerWPF.Viewer
 {
@@ -17,10 +20,29 @@ namespace ResultViewerWPF.Viewer
         public string[] TextLines;
 
         /// <summary>
+        /// Главная панель, на которой будут расположены все TextBlock-и
+        /// </summary>
+        private StackPanel mainPanel = null;
+
+        /// <summary>
         /// Стандартный публичный конструктор
         /// </summary>
         public TextBar()
         {
+            // Инициализируем главную панель
+            mainPanel = new StackPanel()
+            {
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                
+            };
+
+            // Инициализируем TranslateTransform
+            BarTG = new TransformGroup();
+            BarTG.Children.Add(new TranslateTransform());
+            BarTG.Children.Add(new ScaleTransform());
+            mainPanel.RenderTransform = BarTG;
+
 
         }
     }
